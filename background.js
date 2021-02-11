8748f9be3ebb3affaad19e978ae1fb598bb3499a
dwayneRichards <dwaynerichards@gmail.com>
2021-02-11 02:32:56 -0500

    done extenstion

--- background.js
+++ background.js
@@ -1,24 +1,33 @@
 {
-
-
   let color = "#3aa757";
 
-  chrome.runtime.onInstalled.addListener(() => {
+chrome.runtime.onInstalled.addListener(() => {
     chrome.storage.sync.set({ color });
-
     console.log("Default background color set to %cgreen", `color: ${color}`);
     console.log("yoooooo");
 
     chrome.tabs.query({}, (tabs) => {
       for (let i = 0; i < tabs.length; i++) {
         if (tabs[i].audible) {
-          console.log(["hi", tabs[i]]);
-          tabs[i].highlighted = true;
+          console.log(["hi", tabs[i].id]);
+          console.log(tabs[i])
+          //tabs[i].highlighted = true;
           tabs[i].active = true;
-          tabs[i].height = 900;
-          tabs[i].pinned = true;
-        }
+         chrome.tabs.move(tabs[i].id,{index:-1})
+          }
       }
     });
   });
+chrome.tabs.onUpdated.addListener(()=>{
+    chrome.tabs.query({}, (tabs) => {
+      for (let i = 0; i < tabs.length; i++) {
+        if (tabs[i].audible) {
+          console.log(["hi", tabs[i].id]);
+          
+         chrome.tabs.move(tabs[i].id,{index:-1})
+          }
+      }
+    })
+  })
+
 }
