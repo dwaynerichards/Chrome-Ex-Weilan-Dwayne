dd9da59c3abfefd5e454711c232231073c28a05b
dwayneRichards <dwaynerichards@gmail.com>
2021-02-11 00:32:22 -0500

    ok?

--- background.js
+++ background.js
@@ -3,8 +3,17 @@
     //gitaccess to storage
     let color = '#3aa757';
 
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
-});
+  chrome.runtime.onInstalled.addListener(() => {
+    chrome.storage.sync.set({ color });
+    console.log('Default background color set to %cgreen', `color: ${color}`);
+  });
+  chrome.browserAction.onClicked.addListener(function (tab) {
+    goto_tab();
+  });
+
+
+
+;
+
+
 }
